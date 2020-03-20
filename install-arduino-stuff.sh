65f1b14f6ac95529e61a37712be812ac19f0f05c
Steve Coul <steve.coul@icloud.com>
2020-03-20 15:31:24 -0500

    install esp8266 arduino stuff

--- install-arduino-stuff.sh
+++ install-arduino-stuff.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-curl -O https://downloads.arduino.cc/arduino-1.8.12-macosx.zip
-curl -O https://downloads.arduino.cc/arduino-cli/arduino-cli_0.9.0_macOS_64bit.tar.gz
-rm -rf ~/Applications/Arduino.app
-unzip arduino-1.8.12-macosx.zip -d ~/Applications
-tar jxf arduino-cli_0.9.0_macOS_64bit.tar.gz -C ~/Applications/Arduino.app
-~/Applications/Arduino.app/arduino-cli --additional-urls "https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json" core update-index
-~/Applications/Arduino.app/arduino-cli --additional-urls "https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json" core install arduino:samd
-~/Applications/Arduino.app/arduino-cli --additional-urls "https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json" core install SparkFun:samd
+#curl -O https://downloads.arduino.cc/arduino-1.8.12-macosx.zip
+#curl -O https://downloads.arduino.cc/arduino-cli/arduino-cli_0.9.0_macOS_64bit.tar.gz
+#rm -rf ~/Applications/Arduino.app
+#unzip arduino-1.8.12-macosx.zip -d ~/Applications
+#tar jxf arduino-cli_0.9.0_macOS_64bit.tar.gz -C ~/Applications/Arduino.app
+
+#~/Applications/Arduino.app/arduino-cli --additional-urls "https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json" core update-index
+#~/Applications/Arduino.app/arduino-cli --additional-urls "https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json" core install arduino:samd
+#~/Applications/Arduino.app/arduino-cli --additional-urls "https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json" core install SparkFun:samd
+
+~/Applications/Arduino.app/arduino-cli --additional-urls "http://arduino.esp8266.com/stable/package_esp8266com_index.json" core update-index
+~/Applications/Arduino.app/arduino-cli --additional-urls "http://arduino.esp8266.com/stable/package_esp8266com_index.json" core install esp8266:esp8266
 
