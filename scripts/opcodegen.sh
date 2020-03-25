e337cbe1cef1bda552c9320787389e926ec9bcfc
Steve Coul <steve.coul@icloud.com>
2020-03-25 12:45:47 +0000

    fix opcodegen to bash

--- scripts/opcodegen.sh
+++ scripts/opcodegen.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "/* machine generated */" > $2
 echo "#ifndef __opcode_h__" >> $2
 echo "#define __opcode_h__" >> $2
 echo " " >> $2
 echo "typedef struct {" >> $2
-echo "\tint value;" >> $2
-echo "\tconst char* name;" >> $2
+echo "    int value;" >> $2
+echo "    const char* name;" >> $2
 echo "} opcode_lookup_t;" >> $2
 echo " " >> $2
 echo "extern opcode_lookup_t opcode_names[];" >> $2
@@ -29,17 +29,17 @@ while read -r line; do
 			number=$(echo $line | cut -f 1 -d ' ')
 			name=$(echo $line | cut -f 2 -d ' ' )
 
-			echo "\top$name = 0x$number," >> $2
+			echo "    op$name = 0x$number," >> $2
 			echo "$number constant op$name" >> $3
-			echo "\t{ 0x$number, \"op$name\"}," >> $4
+			echo "    { 0x$number, \"op$name\"}," >> $4
 		fi
 	fi
 done < $1
 
-echo "\t{-1,\"\"}};" >> $4
+echo "    {-1,\"\"}};" >> $4
 echo " " >> $4
 
-echo "\topENDOFLIST };" >> $2
+echo "    opENDOFLIST };" >> $2
 echo " " >> $2
 echo "#endif" >> $2
 echo " " >> $2
