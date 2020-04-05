6f62a8aaabff6d6c1240fcb9234ef03635e80239
STEVEN COUL <stevencoul@STEVENs-Mac-Pro.local>
2020-04-05 14:54:59 -0500

    script to get osx tools for arm bare dev. Use them for the samd51 target

---
+++ scripts/get-osx-arm-tools.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf arm-tools
+mkdir arm-tools 
+cd arm-tools
+git clone https://github.com/ARM-software/CMSIS_5.git
+git clone https://github.com/arduino/ArduinoModule-CMSIS-Atmel.git
+curl -L https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-mac.tar.bz2 | tar jxvf
+curl -L https://github.com/shumatech/BOSSA/releases/download/1.9.1/bossa-1.9.1.dmg -O
+hdiutil attach bossa-1.9.1.dmg 
+cp /Volumes/BOSSA/bossac .
+hdiutil detach /Volumes/BOSSA
+
